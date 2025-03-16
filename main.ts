2c17c9c7b9314e5fdb86cfd1e48604cbe5e69d73
Muzafar Umarov <umarov@gmail.com>
2025-03-16 11:30:23 -0400

    Add a few apis

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ router.post(
   },
 );
 
-router.get("/urls", async () => {
+router.get("/urls", async (context) => {
   try {
     const urlKeys = kv.list({ prefix: ["url"] });
     const urls = [];
@@ -45,7 +45,95 @@ router.get("/urls", async () => {
     return { urls };
   } catch (err) {
     console.error(err);
-    return { message: err.message };
+    context.throw(500, err.message);
   }
 });
+
+router.get("/calls/:id/agents", async ({ params }) => {
+  const agents = await getAgents(params);
+
+  return { agents };
+});
+
+router.get("/stream/calls/:id/agents", ({ params }) => {
+  let timer: number;
+  const body = new ReadableStream({
+    start(controller) {
+      timer = setInterval(async () => {
+        try {
+          const agents = await getAgents(params);
+          controller.enqueue(
+            new TextEncoder().encode(JSON.stringify(agents) + "\n"),
+          );
+        } catch (err) {
+          console.error(err);
+        }
+      }, 1000);
+    },
+    cancel() {
+      clearInterval(timer);
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "content-type": "text/plain",
+      "x-content-type-options": "nosniff",
+    },
+  });
+});
+
+router.get("/stream", () => {
+  let timer: number;
+  const body = new ReadableStream({
+    start(controller) {
+      timer = setInterval(() => {
+        const message = `It is ${new Date().toISOString()}\n`;
+        console.log(message);
+        try {
+          controller.enqueue(new TextEncoder().encode(message));
+        } catch (err) {
+          console.error(err);
+        }
+      }, 500);
+    },
+    cancel() {
+      clearInterval(timer);
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "content-type": "text/plain",
+      "x-content-type-options": "nosniff",
+    },
+  });
+});
+
 router.listen({ port: 3000 });
+
+async function getAgents(params: RouteParameters<Path>) {
+  const { id } = params;
+  const urlKeys = kv.list({ prefix: ["url"] });
+  const urls: string[] = [];
+  for await (const entry of urlKeys) {
+    urls.push(entry.value as string);
+  }
+
+  const responses = await Promise.all(
+    urls.map((url) =>
+      fetch(`${url}/calls/${id}/agents`, {
+        headers: { "Content-Type": "application/json" },
+      }),
+    ),
+  );
+
+  const agents = await Promise.all(
+    responses.map((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return null;
+      }
+    }),
+  );
+  return agents.filter((agent) => agent);
+}
